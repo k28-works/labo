f43d9c5740f1ef5ab8902410ea423235c9b0f561
k28 <k28-works@users.noreply.github.com>
2018-03-11 23:33:15 +0900

    jscript追加

---
+++ jscript/jscript.js
@@ -0,0 +1,257 @@
+//
+// jscript.js
+//
+
+Object.prototype.each = function (callback) {
+    for (var key in this) {
+        if (this.hasOwnProperty(key)) {
+            callback.call(this, key, this[key]);
+        }
+    }
+};
+String.prototype.repeat = function (num) {
+    return new Array(num + 1).join(this);
+};
+Array.prototype.each = function (callback) {
+    for (var i = 0; i < this.length; ++i) {
+        callback.call(this, this[i], i);
+    }
+};
+Array.prototype.map = function (callback) {
+    var ret = [];
+    for (var i = 0; i < this.length; ++i) {
+        ret.push(callback.call(this, this[i], i));
+    }
+    return ret;
+};
+Array.prototype.select = function (callback) {
+    var ret = [];
+    for (var i = 0; i < this.length; ++i) {
+        if (callback.call(this, this[i], i)) {
+            ret.push(this[i]);
+        }
+    }
+    return ret;
+};
+
+function x_each(object, callback) {
+    for (var e = new Enumerator(object); !e.atEnd(); e.moveNext()) {
+        callback(e.item());
+    }
+}
+function x_map(object, callback) {
+    var ret = [];
+    for (var e = new Enumerator(object); !e.atEnd(); e.moveNext()) {
+        ret.push(callback(e.item()));
+    }
+    return ret;
+}
+function x_select(object, callback) {
+    var ret = [];
+    for (var e = new Enumerator(object); !e.atEnd(); e.moveNext()) {
+        if (callback(e.item())) {
+            ret.push(e.item());
+        }
+    }
+    return ret;
+}
+
+var console = {};
+var process = {};
+var shell = {};
+var file = {};
+var path = {};
+var using = {};
+(function () {
+    //
+    // console
+    //
+    console.log = function (text) {
+        WScript.StdOut.WriteLine(text);
+    };
+    console.write = function (text) {
+        WScript.StdOut.Write(text);
+    };
+    console.writeln = function (text) {
+        WScript.StdOut.WriteLine(text);
+    };
+    //
+    // process
+    //
+    process.exit = function (error) {
+        WScript.Quit(error || 0);
+    };
+    if (WScript.Arguments.length > 0) {
+        process.source = WScript.Arguments(0);
+        process.argc = WScript.Arguments.length - 1;
+        process.argv = x_select(WScript.Arguments, function () { return true; }).slice(1);
+    }
+    else {
+        process.exit(0);
+    }
+    //
+    // shell
+    //
+    var shell_ = WScript.CreateObject("WScript.Shell");
+    shell.exec = function (command) {
+        var exec_ = shell_.Exec('CMD /C ' + command);
+        var result_ = exec_.StdOut.ReadAll();
+        return {
+            result: result_,
+            error: exec_.ExitCode
+        };
+    };
+    //
+    // file
+    //
+    var fso_ = new ActiveXObject("Scripting.FileSystemObject");
+    file.exists = function (pathspec) {
+        return fso_.FileExists(pathspec) || fso_.FolderExists(pathspec);
+    };
+    //
+    // path
+    //
+    path.separator = '/';
+    path.join = function () {
+        return arguments.join('/').replace(/\\/g, '/').replace(/\/+/g, path.separator);
+    };
+    //
+    // using
+    //
+    var BufferInterface = function (stream) {
+        var stream_ = stream || new ActiveXObject("ADODB.Stream");
+        this.open = function () {
+            stream_.Open();
+        };
+        this.close = function () {
+            stream_.Close();
+        };
+        this.save = function (pathspec) {
+            stream_.SaveToFile(pathspec, file.exists(pathspec) ? 2 : 1);
+        };
+        this.load = function (pathspec) {
+            stream_.LoadFromFile(pathspec);
+        };
+        this.size = function () {
+            return stream_.Size;
+        };
+        this.position = function () {
+            return stream_.Position;
+        };
+        this.seek = function (position) {
+            stream_.Position = position;
+        };
+    }
+    var TextBuffer = function () {
+        var stream_ = new ActiveXObject("ADODB.Stream");
+        var buffer_ = new BufferInterface(stream_);
+        buffer_.open = function () {
+            stream_.Open();
+            stream_.Charset = "utf-8";
+        };
+        buffer_.write = function (text) {
+            stream_.WriteText(text);
+        };
+        buffer_.writeln = function (text) {
+            stream_.WriteText(text, 1);
+        };
+        buffer_.read = function (num) {
+            return stream_.ReadText(num || -1);
+        };
+        buffer_.readln = function () {
+            return stream_.ReadText(-2);
+        };
+        buffer_.save = function (pathspec) {
+            stream_.Position = 0;
+            stream_.Type = 1;
+            stream_.Position = 3;
+            var bytes = stream_.Read();
+            stream_.Position = 0;
+            stream_.Write(bytes);
+            stream_.SetEOS();
+            stream_.SaveToFile(pathspec, file.exists(pathspec) ? 2 : 1);
+            stream_.Type = 2;
+        };
+        return buffer_;
+    };
+    var DataBuffer = function () {
+        var stream_ = new ActiveXObject("ADODB.Stream");
+        var buffer_ = new BufferInterface(stream_);
+        var dom_ = new ActiveXObject("Msxml2.DOMDocument.6.0");
+        var typed_value_ = dom_.createElement("typed_value");
+        typed_value_.dataType = 'bin.hex';
+        buffer_.open = function () {
+            stream_.Open();
+            stream_.Type = 1;
+        };
+        buffer_.write = function (data) {
+            stream_.Write(data);
+        };
+        buffer_.write_bytes = function (bytes) {
+            typed_value_.text = bytes.map(function (value) {
+                return value.toString(16);
+            }).join('');
+            stream.Write(typed_value_.nodeTypedValue);
+        };
+        buffer_.read = function (num) {
+            return stream_.Read(num || -1);
+        };
+        buffer_.read_bytes = function (num) {
+            typed_value_.nodeTypedValue = stream_.Read(num || -1);
+            return (typed_value_.text.match(/.{2}/g) || []).map(function (hex) {
+                return parseInt(hex, 16);
+            });
+        };
+        buffer_.int2hex = function (int, size) {
+            var padding = size ? size * 2 : 8;
+            return ('0'.repeat(padding) + (int >>> 0).toString(16)).slice(-padding);
+        };
+        buffer.hex2bytes = function (hex) {
+            return (hex.match(/.{2}/g)).map(function (byte) {
+                return parseInt(byte, 16);
+            });
+        };
+        return buffer_;
+    };
+    using.text_buffer = function (callback) {
+        var buffer = new TextBuffer();
+        try {
+            buffer.open();
+            return callback(buffer);
+        }
+        finally {
+            buffer.close();
+        }
+    };
+    using.data_buffer = function (callback) {
+        var buffer = new DataBuffer();
+        try {
+            buffer.open();
+            return callback(buffer);
+        }
+        finally {
+            buffer.close();
+        }
+    };
+}());
+
+if (process.source && file.exists(process.source)) {
+    try {
+        eval(using.text_buffer(function (buffer) {
+            buffer.load(process.source);
+            while (buffer.read(1) == '@') {
+                buffer.readln();
+            }
+            buffer.seek(buffer.position() - 1);
+            return buffer.read();
+        }));
+    }
+    catch (e) {
+        console.log(e.name + ': ' + e.message);
+    }
+    finally {
+        process.exit();
+    }
+}
+
+// EOF //
---
+++ jscript/template.bat
@@ -0,0 +1,10 @@
+@CScript //Nologo jscript.wsf "%~f0" %*
+@EXIT /B
+
+//
+// template.bat
+//
+
+console.log(process.argv + '/' + process.argc);
+
+// EOF //
